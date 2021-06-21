7b2260760c39d466e94aee01413809ec1964a014
Balazs Horvath <balazs_horvath@epam.com>
2021-06-21 14:58:30 +0200

    board edit component

--- frontend/src/components/BoardEdit.jsx
+++ frontend/src/components/BoardEdit.jsx
@@ -1,14 +1,58 @@
+import { useFormik } from 'formik'
+import { useDispatch } from 'react-redux'
+import { modifyBoard } from '../store/reducers/boards'
+
 const BoardEdit = ({title, id, setIsEdit}) => {
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setIsEdit(prevState => !prevState)
-  }
+  const dispatch = useDispatch()
+  const formik = useFormik({
+    
+    initialValues: {
+      title,
+      id
+    },
+
+    validate: values => {
+      const error = {}
+      if(!values.title.trim()) {
+        error.title = 'Required!'
+      }
+      return error
+    },
+
+    onSubmit: values => {
+      const { id, title } = values
+      dispatch(modifyBoard({ id, title }))
+      setIsEdit(prevState => !prevState)
+    },
+
+  })
 
   return (
-    <form className='Board__edit-form' onSubmit={handleSubmit}>
-      <input className='Board__edit-input' type="text" value={title} autoFocus/>
-      <button className='btn btn--edit' type='submit'>ok</button>
+    <form
+      className='Board__edit-form'
+      onSubmit={formik.handleSubmit}
+    >
+      <div className="Board__edit-container">
+        <input
+          className='Board__edit-input'
+          type="text"
+          name='title'
+          autoFocus
+          value={formik.values.title}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <label className='Board__edit-error'>
+          {formik.errors.title}
+        </label>
+      </div>
+      <button
+        className='btn btn--edit'
+        type='submit'
+      >
+        ok
+      </button>
     </form>
   )
 }
