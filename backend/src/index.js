9b8fd6415b8ed5bd32b352d44dac89916d1fc270
Balazs Horvath <balazs_horvath@epam.com>
2021-07-12 11:04:02 +0200

    board api actions

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,37 +1,49 @@
 const express = require('express')
 const cors = require('cors')
 const PORT = 5000
+let boards = require('./db/boards.json')
 
 const app = express()
-
 app.use(cors())
+app.use(express.json())
 
-let boards = [
-  {
-    id: 2387645,
-    title: 'My First Board',
-    todo: [
-      {
-        id: 7812635,
-        title: "Task1",
-        description: "Lorem ipsum dolor sit amet consectetur"
-      },
-    ],
-    inProgress: [],
-    done: [],
-  },
-  {
-    id: 1287635,
-    title: 'My Second Board',
-    todo: [],
-    inProgress: [],
-    done: [],
-  },
-] 
 
+// GET ALL BOARDS
 app.get('/api/boards', (req, res) => {
-  res.send(JSON.stringify(boards))
+  res.json(boards)
 })
+
+// GET A BOARD
+app.get('/api/boards/:boardId', (req, res) => {
+  const { boardId } = req.params
+  const requestedBoard = boards.find(board => board.id == boardId)
+  res.json(requestedBoard || {})
+})
+
+// ADD A BOARD
+app.post('/api/boards', (req, res) => {
+  boards.push(req.body)
+  res.json(boards)
+})
+
+// DELETE A BOARD
+app.delete('/api/boards/:boardId', (req, res) => {
+  const { boardId } = req.params
+  boards = boards.filter(board => board.id != boardId)
+  res.json(boards)
+})
+
+// MODIFY A BOARD
+app.put('/api/boards/:boardId', (req, res) => {
+  const { boardId } = req.params
+  const modification = req.body
+
+  boards = boards.map(board =>
+    board.id == boardId ? { ...board, ...modification } : board
+  )
+  res.json(boards)
+})
+
 
 app.listen(PORT, () => {
   console.log('Server runnig on PORT: ' + PORT)
